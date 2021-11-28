e6b1eb14de5293f7dcd1407eb986b7397477495d
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-28 18:54:13 -0300

    Adicionada função alteração container para meme pronto

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const buttonFile = document.getElementById('meme-insert');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 const buttonEarth = document.getElementById('earth');
-const memesProntos = document.getElementById('meme-1-2-3-4');
-const buttons = document.getElementById('buttons');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 function changeBorder(event) {
   const whoClicked = event.target;
@@ -23,3 +25,22 @@ function changeBorder(event) {
 buttonFire.addEventListener('click', changeBorder);
 buttonWater.addEventListener('click', changeBorder);
 buttonEarth.addEventListener('click', changeBorder);
+
+function containerMemePronto(event) {
+  const whoClicked = event.target;
+
+  if (whoClicked.id === 'meme-1') {
+    container.style.backgroundImage = 'url(imgs/meme1.png)';
+  } else if (whoClicked.id === 'meme-2') {
+    container.style.backgroundImage = 'url(imgs/meme2.png)';
+  } else if (whoClicked.id === 'meme-3') {
+    container.style.backgroundImage = 'url(imgs/meme3.png)';
+  } else {
+    container.style.backgroundImage = 'url(imgs/meme4.png)';
+  }
+}
+
+meme1.addEventListener('click', containerMemePronto);
+meme2.addEventListener('click', containerMemePronto);
+meme3.addEventListener('click', containerMemePronto);
+meme4.addEventListener('click', containerMemePronto);
