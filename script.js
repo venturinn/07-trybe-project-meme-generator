5387e9b4e02914c19a2c0abeb2e8cca87ced2c37
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-28 22:31:27 -0300

    Adicionada função obtém imagem externa.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('meme-image-container');
 const textInput = document.getElementById('text-input');
-const text = document.getElementById('meme-text');
+const textMeme = document.getElementById('meme-text');
 const buttonFile = document.getElementById('meme-insert');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
@@ -9,10 +9,10 @@ const meme1 = document.getElementById('meme-1');
 const meme2 = document.getElementById('meme-2');
 const meme3 = document.getElementById('meme-3');
 const meme4 = document.getElementById('meme-4');
+const image = document.getElementById('image');
 
 function changeBorder(event) {
   const whoClicked = event.target;
-
   if (whoClicked.id === 'fire') {
     container.style.border = 'red dashed 3px';
   } else if (whoClicked.id === 'water') {
@@ -30,13 +30,13 @@ function containerMemePronto(event) {
   const whoClicked = event.target;
 
   if (whoClicked.id === 'meme-1') {
-    container.style.backgroundImage = 'url(imgs/meme1.png)';
+    image.src = 'imgs/meme1.png';
   } else if (whoClicked.id === 'meme-2') {
-    container.style.backgroundImage = 'url(imgs/meme2.png)';
+    image.src = 'imgs/meme2.png';
   } else if (whoClicked.id === 'meme-3') {
-    container.style.backgroundImage = 'url(imgs/meme3.png)';
+    image.src = 'imgs/meme3.png';
   } else {
-    container.style.backgroundImage = 'url(imgs/meme4.png)';
+    image.src = 'imgs/meme4.png';
   }
 }
 
@@ -44,3 +44,18 @@ meme1.addEventListener('click', containerMemePronto);
 meme2.addEventListener('click', containerMemePronto);
 meme3.addEventListener('click', containerMemePronto);
 meme4.addEventListener('click', containerMemePronto);
+
+function text() {
+  textMeme.innerText = textInput.value;
+}
+
+textInput.addEventListener('keyup', text);
+
+//Ref.:https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+//Ref.:https://pt.stackoverflow.com/questions/42900/como-posso-pegar-o-caminho-do-input-file
+
+function localFile(event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+buttonFile.addEventListener('change', localFile);
