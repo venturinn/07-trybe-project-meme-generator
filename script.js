11f60252be61c0b239a1996719a0a6128e2cafb4
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-28 23:00:16 -0300

    Corrigido erros nomes IDs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const meme1 = document.getElementById('meme-1');
 const meme2 = document.getElementById('meme-2');
 const meme3 = document.getElementById('meme-3');
 const meme4 = document.getElementById('meme-4');
-const image = document.getElementById('image');
+const image = document.getElementById('meme-image');
 
 function changeBorder(event) {
   const whoClicked = event.target;
@@ -51,8 +51,9 @@ function text() {
 
 textInput.addEventListener('keyup', text);
 
-//Ref.:https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
-//Ref.:https://pt.stackoverflow.com/questions/42900/como-posso-pegar-o-caminho-do-input-file
+// Ref.:https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+// Ref.:https://pt.stackoverflow.com/questions/42900/como-posso-pegar-o-caminho-do-input-file
+// Ref.:https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 
 function localFile(event) {
   image.src = URL.createObjectURL(event.target.files[0]);
